73b9d4b643fc88a35d13f8306e3f536610c8aacf
Abrorbek Ismoilov <abror2142@gmail.com>
2024-11-23 13:19:02 +0500

    Custom User and User Info Fields

--- news/accounts/admin.py
+++ news/accounts/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User, UserInfo
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['id', 'username', 'email']
+    list_display_links = ['username']
+
+
+@admin.register(UserInfo)
+class UserInfoAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'first_name', 'last_name']
+    list_display_links = ['user']
--- news/accounts/models.py
+++ news/accounts/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    first_name = None
+    last_name = None
+
+
+class UserInfo(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    first_name = models.CharField(max_length=150, blank=True, null=True)
+    middle_name = models.CharField(max_length=150, blank=True, null=True)
+    last_name = models.CharField(max_length=150, blank=True, null=True)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
+    date_of_birth = models.DateField(null=True, blank=True)
+    image = models.ImageField(upload_to='user/', blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.user} -> {self.first_name} {self.last_name}"
+
+
